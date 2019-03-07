6c1c8feda4c44fff0d0973da9fcd9beb9ac44741
Joseph Watts <josephcw@live.com>
2019-03-06 21:50:22 -0600

    Add base methods

---
+++ knapsack.py
@@ -0,0 +1,21 @@
+
+class Item:
+    def __init__(self, weight, value):
+        self.weight = weight
+        self.value = value
+    
+    def __repr__(self):
+        return 'Weight: {}, Value: {}'.format(self.weight, self.value)
+
+def knapsack_dynamic():
+    pass
+
+def knapsack_decreasing_value():
+    pass
+
+def knapsack_increasing_weight():
+    pass
+
+
+item1 = Item(4, 5)
+print(item1)
