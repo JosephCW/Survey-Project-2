daa16205474453d7fb26f4959c66947970c7bb6e
Daiwei Chen <kinbd8@gmail.com>
2019-03-13 20:01:55 -0500

    Finialized code for production. Got rid of debugging print statements and added header.

--- knapsack.py
+++ knapsack.py
@@ -1,3 +1,10 @@
+#!/bin/python3
+
+# 0-1 Knapsack Python Implementation and Analyzer
+# Authors: Daiwei Chen  (@ForeverAnApple)
+#          Joseph Watts (@JosephCW)
+# Version: 2019-03-13
+
 import argparse
 import time
 import random
@@ -16,42 +23,31 @@ def knapsack_dynamic(items, maximum_weight):
     # 2d array with n rows and n+1 columns
     # knapsack_table at [i][j] represents optimal value with # of items being i, and j representing maximum weight
     # Note, this is a VERY LARGE array.
-    #knapsack_table = [[0 for x in range(n)] for x in range(maximum_weight+1)]
-    #for r in range(n):
-        #print(str(r))
+    knapsack_table = [[0 for i in range(maximum_weight + 1)]
+                      for y in range(n)]
 
-    knapsack_table = [[0 for i in range(maximum_weight + 1)] for y in range(n)]
-
-    #print(str(len(knapsack_table)))
     for i in range(n):
         for j in range(maximum_weight + 1):
             knapsack_table[i][0] = 0
-            #print("i:", i, "j:", j)
             knapsack_table[0][j] = 0 if j < items[0].weight else items[0].value
     for i in range(n):
         for j in range(maximum_weight + 1):
             if j < items[i].weight:
                 knapsack_table[i][j] = knapsack_table[i-1][j]
             else:
-                #print(str(items[i].value))
-                #print(knapsack_table[j-1][j-items[i].weight])
                 knapsack_table[i][j] = max(knapsack_table[i-1][j], knapsack_table[i-1][j-items[i].weight] + items[i].value)
 
-    #print(str(knapsack_table[n-1][maximum_weight]))
     return knapsack_table[n-1][maximum_weight]
 
 def knapsack_greedy_grab(items, max_wt):
     sigmaValue = 0
-    #print("\nGrabbing from list:", items)
+
     for item in items:
-        #print("Checking item:", item)
         if item.weight <= max_wt:
-            #print("Grabbing:", item, "\nTotal value:", sigmaValue)
             max_wt -= item.weight
             sigmaValue += item.value
         if max_wt == 0:
             return sigmaValue
-    #print()
     return sigmaValue
 
 # Hi I'm Greedy Greedman, and I'm a collector of interesting trinkets of
@@ -60,7 +56,6 @@ def knapsack_greedy_weight(items, max_wt, decreasing=False):
     items_sorted = sorted(items,
                           key=lambda item: item.weight,
                           reverse=decreasing)
-    #print("weight sorted:", items_sorted)
     return knapsack_greedy_grab(items_sorted, max_wt)
 
 # Sup y'all it's Greedy Greedman coming at cha, once again we have another
@@ -71,9 +66,7 @@ def knapsack_greedy_value(items, max_wt):
     items_sorted = sorted(items,
                           key=lambda item: item.value,
                           reverse=True)
-    #print("value sorted:", items_sorted)
     return knapsack_greedy_grab(items_sorted, max_wt)
-
 
 # Hey guys, Greedy Greedman here once again, coming at ya with another
 # house tour, it's not my house but the owners were nice enough to leave
@@ -82,7 +75,6 @@ def knapsack_greedy_ratio(items, max_wt):
     items_sorted = sorted(items,
                           key=lambda item: item.value/item.weight,
                           reverse=True)
-    #print("ratio sorted:", items_sorted)
     return knapsack_greedy_grab(items_sorted, max_wt)
 
 # wt and value are tuples of (min_value, max_value)
@@ -143,17 +135,7 @@ if __name__ == '__main__':
             greed_out = greed[j](items, maxWeight)
             times[j].append(time.perf_counter_ns() - start)
             errs[j].append(greed_out / optimal)
-    '''
-    dynamic = knapsack_dynamic(items, maxWeight)
-    weight = knapsack_greedy_weight(items, maxWeight)
-    value = knapsack_greedy_value(items, maxWeight)
-    ratio = knapsack_greedy_ratio(items, maxWeight)
 
-    print("Dynamic answer:", dynamic)
-    print("Greedy weight answer:", weight)
-    print("Greedy value answer:", value)
-    print("Greedy ratio answer:", ratio)
-    '''
     # Output results
     print("Algorithms tested: DP, Increasing Weight, Decreasing Value, and Decreasing Ratio (value/weight)\n\nAll times are recorded and shown in nanoseconds.\nSorting times are included within runtime.\n")
     algos = ["Increasing Weight", "Decreasing Value", "Decreasing Ratio (value/weight)"]
