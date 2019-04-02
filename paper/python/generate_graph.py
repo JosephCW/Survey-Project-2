00dd7319ffcfaf604692f6f464378182a51bad23
Joseph Watts <josephcw@live.com>
2019-04-02 17:17:58 -0500

    update for dave

--- paper/python/generate_graph.py
+++ paper/python/generate_graph.py
@@ -1,23 +1,55 @@
 import matplotlib.pyplot as plt
 
-num_runs = [1, 5, 10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 750, 1000, 1750, 2500, 3500]
-forced = [0.000000033, 0.000000085, 0.000000448, 0.000030930, 0.000194367, 0.000594095, 0.001366055, 0.002607847, 0.004420136, 0.006950275, 0.010421218, 0.014655456, 0.020030729, 0.067125954, 0.157617270, 0.837418008, 2.432867191, 6.606255604]
-kadane = [0.000000025, 0.000000036, 0.000000051, 0.000000125, 0.000000184, 0.000000260, 0.000000331, 0.000000402, 0.000000466, 0.000000573, 0.000000639, 0.000000716, 0.000000956, 0.000001231, 0.000001779, 0.000002682, 0.000003601, 0.000005064]
+# For both graphs
+max_weights = [100, 200, 300, 400, 500]
+# For the max weight vs time graph
+dynamic_timings = [n / 100 for n in [7446320, 14733273, 24533937, 29605866, 38435691]]
+increasing_weight_timings = [n / 100 for n in [31775, 35600, 50913, 41883, 46523]]
+decreasing_value_timings = [n / 100 for n in [22007, 25499, 32682, 32846, 36866]]
+decreasing_ratio_timings = [n / 100 for n in [30532, 33547, 41684, 33837, 42071]]
 
-dynamic_times = []
-decreasing_value_times = []
-increasing_weight_times = []
-decreasing_value_weight_ratio_times = []
+# For the second graph of max weight vs accuracy 
+dynamic_accuracy = [100.0, 100.0, 100.0, 100.0, 100.0]
+increasing_weight_accuracy = [84.3615, 85.9055, 87.1212, 89.4167, 95.5788]
+decreasing_value_accuracy = [73.1946, 88.6910, 95.0205, 98.4185, 99.8065]
+decreasing_ratio_accuracy = [99.8515, 99.8826, 99.9342, 99.9409, 99.9844]
+
+f, (ax, ax2) = plt.subplots(2, 1, sharex=True)
+
+line_dynamic_timings = ax.plot(max_weights, dynamic_timings, label="Dynamic Algorithm", linewidth=2)
+line_increasing_weight = ax2.plot(max_weights, increasing_weight_timings, label="Increasing Weight", linewidth=2)
+line_decreasing_value = ax2.plot(max_weights, decreasing_value_timings, label="Decreasing Value", linewidth=2)
+line_decreasing_ratio = ax2.plot(max_weights, decreasing_ratio_timings, label="Decreasing Ratio", linewidth=2)
 
 
-line_forced, = plt.plot(num_runs, forced, label='Brute Force Algorithm', linewidth=4) 
-line_kadane, = plt.plot(num_runs, kadane, label='Kadane\'s Algorithm', linewidth=5)
-plt.legend(handles=[line_forced, line_kadane])
+#plt.legend(handles=[line_dynamic_timings[0], line_increasing_weight[0], line_decreasing_value[0], line_decreasing_ratio[0]])
+
+# Add dashes on side of graph
+d = .015  # how big to make the diagonal lines in axes coordinates
+# arguments to pass to plot, just so we don't keep repeating them
+kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+
+kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
+
+
+ax.set_ylim(70000, 600000) # outliers
+ax2.set_ylim(200, 600) # most of the data
+# hide the axis between the two
+ax.spines['bottom'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+ax.xaxis.tick_top()
+ax.tick_params(labeltop='off')
+ax2.xaxis.tick_bottom()
+
 
 # axis(xmin, xmax, ymin, ymax)
-plt.axis([0, 2750, 0, 4])
-plt.ylabel('Average Time in Seconds') 
-plt.xlabel('Size of Array')
-plt.title('Time to Calculate Maximum Sum in Array of Size N') 
+#plt.axis([100, 500, 0, 500000])
+plt.ylabel('Average Time in ms') 
+plt.xlabel('Maximum Weight')
+ax.set_title('Time to Calculate Items to take with Maximum Available Weight (N)') 
 #plt.show()
-plt.savefig('avgTimeGraph.png')
+plt.savefig('timeGraph.png')
